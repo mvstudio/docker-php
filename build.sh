b1690e5ea264997bb972f825e925f003ddbc1322
WASTIAU Sébastien <sebastien@hexeo.be>
2023-02-16 16:02:06 +0100

    fix image name [skip ci]

--- build.sh
+++ build.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
 docker build \
-  -t mvstudio/php:7-alpine \
-  -t mvstudio/php:7.0-alpine \
-  -t mvstudio/php:7.2-alpine \
-  -t mvstudio/php:7.3-alpine \
-  -t mvstudio/php:7.4-alpine \
-  -t mvstudio/php:7.2-alpine-wordpress \
-  -t mvstudio/php:7.4-alpine-wordpress \
+  -t ghcr.io/mvstudio/php:7-alpine \
+  -t ghcr.io/mvstudio/php:7.0-alpine \
+  -t ghcr.io/mvstudio/php:7.2-alpine \
+  -t ghcr.io/mvstudio/php:7.3-alpine \
+  -t ghcr.io/mvstudio/php:7.4-alpine \
+  -t ghcr.io/mvstudio/php:7.2-alpine-wordpress \
+  -t ghcr.io/mvstudio/php:7.4-alpine-wordpress \
   ./7-alpine
 
 docker build \
-  -t mvstudio/php:8-alpine \
-  -t mvstudio/php:8.0-alpine-wordpress \
+  -t ghcr.io/mvstudio/php:8-alpine \
+  -t ghcr.io/mvstudio/php:8.0-alpine-wordpress \
   ./8-alpine
+
+docker build \
+  -t ghcr.io/mvstudio/php:81-alpine \
+  ./81-alpine
+
+docker build \
+  -t ghcr.io/mvstudio/php:82-alpine \
+  ./82-alpine
